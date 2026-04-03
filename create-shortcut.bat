60ae5a47b8403d2ed7597810da7062819a663ec5
nowyback <omegacooperationsystems@gmail.com>
2026-04-03 13:20:59 +0200

    V0.8.3 - Bug Fixes

--- create-shortcut.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Creating desktop shortcut for Todoodle...
-powershell "$WshShell = New-Object -comObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut('%USERPROFILE%\Desktop\Todoodle.lnk'); $Shortcut.TargetPath = '%CD%\start.bat'; $Shortcut.WorkingDirectory = '%CD%'; $Shortcut.IconLocation = '%CD%\src\icon.ico,0'; $Shortcut.Save()"
-echo Desktop shortcut created! You can now double-click "Todoodle" on your desktop.
-pause
